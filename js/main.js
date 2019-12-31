73f8f0c78076bd3749f2a415be60020ec57ff384
Vladislav Mishchenko <55705767+vmishchenko@users.noreply.github.com>
2019-12-31 10:30:33 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const dayTime = (hours >= 8 && hours <= 16) ? 'morning' : 'night';
   const randomImageId = Math.floor(Math.random() * 3 + 1);
 
-  document.getElementById("showBg").style.backgroundImage = `url(\"/img/bgs/${dayTime}/${randomImageId}${screenType}\")`;
+  document.getElementById("showBg").style.backgroundImage = `url(\"./img/bgs/${dayTime}/${randomImageId}${screenType}\")`;
   // document.getElementById("showBg").style.color = 'red';
 });
